e52ad5a832408fd39983059b91be7a4fc7884685
Rania <88149988+raniagr1@users.noreply.github.com>
2022-05-10 09:40:48 +0100

    Update T1Quest.dart

--- lib/pages/T1Quest.dart
+++ lib/pages/T1Quest.dart
@@ -1,8 +1,8 @@
 
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import 'package:Hummy/bienvenue.dart';
-import 'package:Hummy/themes.dart';
+import 'package:Hummy/pages/bienvenue.dart';
+import 'package:Hummy/pages/themes.dart';
 class PetDej1 extends StatefulWidget {
   const PetDej1({ Key? key }) : super(key: key);
 
