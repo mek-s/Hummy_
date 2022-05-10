4b86d43a268419ba24deaab42c56c67686f3cdca
Rania <88149988+raniagr1@users.noreply.github.com>
2022-05-10 09:38:12 +0100

    Update T13Quest.dart

--- lib/pages/T13Quest.dart
+++ lib/pages/T13Quest.dart
@@ -2,8 +2,8 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:try1/pages/bienvenue.dart';
-import 'package:try1/pages/themes.dart';
+import 'package:Hummy/pages/bienvenue.dart';
+import 'package:Hummy/pages/themes.dart';
 
 
 class ChoixPetDej1 extends StatefulWidget {
